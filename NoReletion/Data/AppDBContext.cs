69549c0b5b236fa51562e1c189f414e471b0d641
O'tkirbek Sobirjonov <74007674+SobirjonovSodiqjonovich@users.noreply.github.com>
2022-02-25 23:23:13 +0500

    Install Nuget packages Create AppDBContext Create Entities

---
+++ NoReletion/Data/AppDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoReletion.Entities;
+
+namespace NoReletion.Data
+{
+    /// <summary>
+    /// This class is important for using EF Core
+    /// internal acces modifier recomended
+    /// because we us interfaces and don't use it another project
+    /// </summary>
+    internal class AppDBContext:DbContext
+    {
+        // This is for Database Tables
+        // EF Core recognize tables with DBSet
+        public DbSet<User> Users { get; set; }
+
+        // This mehtod override is nesessary for connect to database
+        // we need to specify a path to connect to the database 
+        // and 
+        // we need to enter which database type to use
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server = (localdb)\\MSSQLLocalDB; Database=EFCoreLearnDB; Trusted_Connection=True;";
+            
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ NoReletion/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoReletion.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+    }
+}
